97cd49bf769654779a9d48b05e9596b051292328
jgphilpott <jacob.gary.philpott@gmail.com>
2016-11-30 18:19:58 -0500

    Added Some Validations and Error Messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ActiveRecord::Base
 
   has_many :projects
   has_many :pledges
+
+  validates :password_confirmation, presence: true, on: :create
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true
   validates :password, length: { minimum: 8 }, on: :create
   validates :password, confirmation: true, on: :create
-  validates :password_confirmation, presence: true, on: :create
 
-  validates :email, uniqueness: true
 end
