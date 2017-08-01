b2650d3400a8e5e1f1139c4290f836ec465e7f08
Stanley Ma <stanley.ma@live.com>
2017-08-01 18:50:00 -0400

    Fix all of the failing tests

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,11 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+  validate :owner?
+
+  def owner?
+    if user == project.owner
+      errors.add(:user, "must not be owner of project")
+    end
+  end
 end
