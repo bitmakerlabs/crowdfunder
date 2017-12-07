e80fc3b4835d742f4a079db5cf06da54c9263197
Situ William <situ_william@yahoo.com>
2017-12-07 17:42:24 -0500

    added validation for pledges

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,14 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+  validate :pledge_userid_cannot_be_project_userid
+
+  def pledge_userid_cannot_be_project_userid
+  	project_users = Project.find_by(user_id: (self.user_id))
+ 	if project_users != nil
+  		if self.project_id == project_users.id && self.user_id == project_users.user_id
+  			errors.add(:user_id, "can't pledge your own project.")
+  		end 
+	end
+  end
 end
