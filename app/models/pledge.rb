9262fdc69eb146c2424db5ce90d99f94bd5f5273
Situ William <situ_william@yahoo.com>
2017-12-11 10:20:07 -0500

    fixed pledge

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -7,11 +7,10 @@ class Pledge < ApplicationRecord
   validate :pledge_userid_cannot_be_project_userid
 
   def pledge_userid_cannot_be_project_userid
-  	project_users = Project.find_by(user_id: (self.user_id))
- 	if project_users != nil
-  		if self.project_id == project_users.id && self.user_id == project_users.user_id
+		if project.user == user 
   			errors.add(:user_id, "can't pledge your own project.")
   		end 
-	end
+	
   end
+
 end
