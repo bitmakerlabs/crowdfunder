1cd6c8e58c48a6353dac6b1c8df75d5cfba194ea
Situ William <situ_william@yahoo.com>
2017-12-12 17:32:17 -0500

    added comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212212011) do
+ActiveRecord::Schema.define(version: 20171212221232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "project_id"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pledges", id: :serial, force: :cascade do |t|
     t.integer "user_id"
